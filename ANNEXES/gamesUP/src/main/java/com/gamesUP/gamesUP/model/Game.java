b09eadd681c3a923c3e53ebe3d8f3d697ce3a457
akramescheba <meschebajordy@gmail.com>
2025-05-17 09:21:35 +0200

    Configuration du model Game.java

--- ANNEXES/gamesUP/src/main/java/com/gamesUP/gamesUP/model/Game.java
+++ ANNEXES/gamesUP/src/main/java/com/gamesUP/gamesUP/model/Game.java
@@ -1,11 +1,12 @@
 package com.gamesUP.gamesUP.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -15,60 +16,80 @@ public class Game {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 
+
 private Long id;
 private String nom;
 private String auteur;
 private String genre;
 private Long numEdition;
+
 @ManyToOne
+
+@JoinColumn(name = "category_id")
+@JsonBackReference
 private Category category;
+
+
 @ManyToOne
 private Publisher publisher;
 
 
+
  //Ajout des getters et setters
-    public Long getId() {
+public Long getId() {
 	return id;
 }
+
 public void setId(Long id) {
 	this.id = id;
 }
+
 public String getNom() {
 	return nom;
 }
+
 public void setNom(String nom) {
 	this.nom = nom;
 }
+
 public String getAuteur() {
 	return auteur;
 }
+
 public void setAuteur(String auteur) {
 	this.auteur = auteur;
 }
+
 public String getGenre() {
 	return genre;
 }
+
 public void setGenre(String genre) {
 	this.genre = genre;
 }
+
 public Long getNumEdition() {
 	return numEdition;
 }
+
 public void setNumEdition(Long numEdition) {
 	this.numEdition = numEdition;
 }
+
 public Category getCategory() {
 	return category;
 }
+
 public void setCategory(Category category) {
 	this.category = category;
 }
+
 public Publisher getPublisher() {
 	return publisher;
 }
+
 public void setPublisher(Publisher publisher) {
 	this.publisher = publisher;
 }
 
-   
 }
