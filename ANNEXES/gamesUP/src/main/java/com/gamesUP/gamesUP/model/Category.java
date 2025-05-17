95043e29cdc51f6a4f6ba6fef5506b902292dfae
akramescheba <meschebajordy@gmail.com>
2025-05-17 09:22:09 +0200

    Configuration du model Category.java

--- ANNEXES/gamesUP/src/main/java/com/gamesUP/gamesUP/model/Category.java
+++ ANNEXES/gamesUP/src/main/java/com/gamesUP/gamesUP/model/Category.java
@@ -1,18 +1,30 @@
 package com.gamesUP.gamesUP.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "categorie")
+@Table(name = "category")
 public class Category {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	String type;
-	 //Ajout des getters et setters
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String type;
+
+	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+	@JsonManagedReference
+	private List<Game> games;
+	
+//Ajout des getters et setters
 	public String getType() {
 		return type;
 	}
@@ -20,4 +32,20 @@ public class Category {
 	public void setType(String type) {
 		this.type = type;
 	}
+
+	public List<Game> getGames() {
+		return games;
+	}
+
+	public void setGames(List<Game> games) {
+		this.games = games;
+	}
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 }
